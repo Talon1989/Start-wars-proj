626050a890294dbb811c6a76747662d1ed999af9
Talon <asd@asd.com>
2018-01-15 20:36:17 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Star Wars Application';
+  testName = 'fabio';
 }
