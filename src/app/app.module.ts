8c346fd5f0d2d5a31ce63ec489f5c1ad7a28ca27
Talon <asd@asd.com>
2018-01-16 17:31:09 +0100

    added 'create-character' from page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 import {StarWarsService} from './star-wars.service';
 import {LogService} from './log.service';
+import { CreateCharacterComponent } from './create-character/create-character.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import {LogService} from './log.service';
     AppComponent,
     TabsComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    CreateCharacterComponent
   ],
   imports: [
     BrowserModule
