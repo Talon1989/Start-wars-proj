55a7aef6aba2994c5541aaf76200b8fad49b0f69
Talon <asd@asd.com>
2018-01-20 19:54:00 +0100

    added child-routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,14 @@ import { HeaderComponent } from './header/header.component';
 import {RouterModule} from '@angular/router';
 
 const routes = [
-  {path: '', component: TabsComponent},
+  {path: 'characters', component: TabsComponent
+    , children: [
+    {path: '', redirectTo: 'all', pathMatch: 'full'},
+    {path: ':side', component: ListComponent}
+    ]
+  },
   {path: 'new-character', component: CreateCharacterComponent},
-  {path: '**', redirectTo: '/'} // catch all unhandled routes and redirect to '/'
+  {path: '**', redirectTo: '/characters'} // catch all unhandled routes and redirect to '/'
 ]
 
 @NgModule({
